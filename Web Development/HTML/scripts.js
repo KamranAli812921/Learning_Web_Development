c3ed054ec9c30e0e856e45ba4dda8dd4ac4872b4
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-02 21:05:54 +0500

    var

--- Web Development/HTML/scripts.js
+++ Web Development/HTML/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hello web");
+console.log("Its time to learn about variables");
+var num1=6;
+var num2=5;
+var num3=num1+num2;
+console.log(num1+"+"+num2+"="+num3);
+if(true){
+    num3=5;
+    console.log(num3);
+}
