42ea5cff159ca99e4b178537f3a4f24ce4185fcb
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-04 08:19:21 +0500

    for loop

--- Web Development/Javascript/04_loops.js
+++ Web Development/Javascript/04_loops.js
@@ -1,11 +1,14 @@
-let a =0
-while(a<10)
-{
+let a = 0
+while (a < 10) {
     console.log(a)
     a++
 }
-a=0
-do{
+a = 0
+do {
     console.log(a)
     a++
-}while(a<10)
+} while (a < 10)
+
+for (let i = 0; i < 10; i++) {
+    console.log(i)
+}
