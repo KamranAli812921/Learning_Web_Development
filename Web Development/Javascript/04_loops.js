3a60249484f6a875b6d822c428b9fba1f7e97d6c
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-04 08:28:24 +0500

    ForIN and ForOf loops

--- Web Development/Javascript/04_loops.js
+++ Web Development/Javascript/04_loops.js
@@ -12,3 +12,17 @@ do {
 for (let i = 0; i < 10; i++) {
     console.log(i)
 }
+let object={
+    "name":"Kamran Ali",
+    "Father Name":"Muhammad Ali",
+    "Age":21
+}
+
+for (const key in object) {
+   console.log(key+":"+object[key])
+
+}
+name ="Kamran"
+for (const element of name) {
+    console.log(element)
+}
