e1983f01823e974d6d9bf4750ce57fd8c37bb444
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-04 12:16:47 +0500

    Basic Calculator

---
+++ Web Development/12/scripts.js
@@ -0,0 +1,20 @@
+let num1 = Number(prompt("Enter first number:"))
+let oprator = prompt("Enter oprator:")
+let num2 = Number(prompt("Enter first number:"))
+
+if (oprator == "-") {
+    alert(`${num1} ${oprator} ${num2}=${num1 - num2}`)
+}
+else if (oprator == "*") {
+    alert(`${num1} ${oprator} ${num2}=${num1 * num2}`)
+}
+else if (oprator == "/") {
+    alert(`${num1} ${oprator} ${num2}=${num1 / num2}`)
+}
+else if (oprator == "+") {
+    alert(`${num1} ${oprator} ${num2}=${num1 + num2}`)
+}
+else
+{
+    alert("Wrong oprator selected")
+}
