14a86c39ad44d65cc3d2c579d26f5bed2a489209
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-04 07:51:31 +0500

    Finding Greater number from 3

--- Web Development/Javascript/03_functions.js
+++ Web Development/Javascript/03_functions.js
@@ -1,7 +1,21 @@
-function sum(a,b)
-{
-    return a+b
+function sum(a, b) {
+    return a + b
 }
 
-console.log(sum(3,4))
+console.log(sum(3, 4))
+function greaterNUm(a, b, c) {
+    if (a > b && a > c) 
+        {
+        return a;
+    }
+    else if (b > a && b > c)
+    {
+        return b
+    }
+    else
+    {
+        return c;
+    }
 
+}
+console.log("The Greatest number is: "+ greaterNUm(1,2,3))
