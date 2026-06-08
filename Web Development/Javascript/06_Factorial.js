5d3a3e67dad7e518178eb12b683a49017928e281
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-08 20:47:28 +0500

    Now take user input to find factorial

--- Web Development/Javascript/06_Factorial.js
+++ Web Development/Javascript/06_Factorial.js
@@ -1,6 +1,14 @@
-let number=5,facto=1
+let number=9,facto=1
 for ( let index=number;index>0;index--)
 {
     facto*=index
 }
 console.log(facto)
+
+let num=Number(prompt("Enter number: "))
+let factorial=1
+for(let i=num;i>0;i--)
+{
+    factorial*=i
+}
+alert(`The Factorial of ${num} is ${factorial}`)
