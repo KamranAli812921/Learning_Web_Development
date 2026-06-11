56c5466daf36b238907c84ae948b017ae6ba3095
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-11 20:11:13 +0500

    Targeted Elements by class name

---
+++ Web Development/13/scripts.js
@@ -0,0 +1,7 @@
+let a = document.getElementsByClassName("box")
+console.log(a)
+
+a[2].style.backgroundColor="red"
+a[0].style.backgroundColor="black"
+a[0].style.color="white"
+a[1].style.backgroundColor="green"
