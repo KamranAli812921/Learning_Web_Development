dd702b90c8e0bdf607150a39a50828a653ce66d6
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-12 10:03:06 +0500

    Outer Html

--- Web Development/13/scripts.js
+++ Web Development/13/scripts.js
@@ -9,3 +9,4 @@ a[1].style.backgroundColor="green"
 document.getElementById("gray").style.backgroundColor="gray"
 console.log(document.querySelector(".box").innerHTML)
 console.log(document.querySelector(".container").innerHTML)
+console.log(document.querySelector(".container").outerHTML)
