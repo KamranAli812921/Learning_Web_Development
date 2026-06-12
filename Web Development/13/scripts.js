561d8ade0e2c3ff7272213c9d75cc75c73c43a88
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-12 10:08:17 +0500

    changing inner html

--- Web Development/13/scripts.js
+++ Web Development/13/scripts.js
@@ -1,13 +1,14 @@
 let a = document.getElementsByClassName("box")
 console.log(a)
 
-a[2].style.backgroundColor="red"
-a[0].style.backgroundColor="black"
-a[0].style.color="white"
-a[1].style.backgroundColor="green"
+a[2].style.backgroundColor = "red"
+a[0].style.backgroundColor = "black"
+a[0].style.color = "white"
+a[1].style.backgroundColor = "green"
 
-document.getElementById("gray").style.backgroundColor="gray"
+document.getElementById("gray").style.backgroundColor = "gray"
 console.log(document.querySelector(".box").innerHTML)
 console.log(document.querySelector(".container").innerHTML)
 console.log(document.querySelector(".container").outerHTML)
 console.log(document.querySelector(".container").tagName)
+document.querySelector(".box").innerHTML="I am Kamran"
