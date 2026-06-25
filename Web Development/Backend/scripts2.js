b9a16e79320ebce94fe6eb1c1186c63677bde86c
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-25 12:28:56 +0500

    using server side js with slug

--- Web Development/Backend/scripts2.js
+++ Web Development/Backend/scripts2.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>');
+app.get('/contact/:slug', (req, res) => {
+  res.send(`<h1>Hello World!    ${req.params.slug}</h1>`);
 });
 
 app.listen(port, () => {
