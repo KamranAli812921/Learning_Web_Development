0fef8a8112674a25dee743669b605707904beaa6
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-26 22:55:10 +0500

    its first webpage of backend

--- Web Development/Backend/scripts2.js
+++ Web Development/Backend/scripts2.js
@@ -9,4 +9,5 @@ app.get('/contact/:slug', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+  console.log(`It my first web page using backend`);
 });
