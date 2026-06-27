f9c9151ff88a24ad2bb100bf4a12fcff024d7180
Kamran Ali <kamranroonjha94@gmail.com>
2026-06-27 20:35:57 +0500

    learning requeste concepts

---
+++ Web Development/Backend/requests.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(express.static('public'))
+
+app.get('/hello/:slug', (req, res) => {
+  res.send(`<h1>Hello Kamran Ali!    ${req.params.slug}</h1>`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
